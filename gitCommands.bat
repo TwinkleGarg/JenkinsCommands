41a2e470a6c785913d7f8fd0c43292947773e57f
Twinkle Garg <Twinkle_Garg@epam.com>
2017-05-03 17:52:36 +0530

    modified

--- gitCommands.bat
+++ gitCommands.bat
@@ -1,5 +1,5 @@
 cd %WORKSPACE%/%Git_Folder%
-git clone https://github.com/%Git_User%/%Git_Repository%.git .
+rem git clone https://github.com/%Git_User%/%Git_Repository%.git .
 git checkout .
 git pull 
 git submodule add -b %Devops_Branch% https://github.com/%Git_User%/%Devops_Repository%.git %Devops_Folder%
