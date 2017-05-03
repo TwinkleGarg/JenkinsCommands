e97a5944081f420b6ce6465b9df3b1a7b39efa15
Twinkle Garg <Twinkle_Garg@epam.com>
2017-05-03 17:49:52 +0530

    modified

--- gitCommands.bat
+++ gitCommands.bat
@@ -2,7 +2,7 @@ cd %WORKSPACE%/%Git_Folder%
 git clone https://github.com/%Git_User%/%Git_Repository%.git .
 git checkout .
 git pull 
-git submodule add -b %Devops_Branch% https://github.com/%Git_User%/%Devops_Repository%.git
+git submodule add -b %Devops_Branch% https://github.com/%Git_User%/%Devops_Repository%.git %Devops_Folder%
 git submodule update --recursive
 git submodule foreach git pull orgin %Devops_Branch%
 git clone 
